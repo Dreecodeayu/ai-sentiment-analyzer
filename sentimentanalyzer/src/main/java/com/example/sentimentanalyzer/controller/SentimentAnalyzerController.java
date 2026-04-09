81170c52290e69efbbf6841fac268dcdec0d8fd3
ELCEFSD <ayush.pathak00322@gmail.com>
2026-04-09 14:52:13 +0530

    changing cross origins

--- sentimentanalyzer/src/main/java/com/example/sentimentanalyzer/controller/SentimentAnalyzerController.java
+++ sentimentanalyzer/src/main/java/com/example/sentimentanalyzer/controller/SentimentAnalyzerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class SentimentAnalyzerController {
 
     private final SentimentAnalyzerService sentimentanalyzerService;
